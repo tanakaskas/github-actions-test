28074c36829dcb2a1c9b036558011fec208bb427
tanakaskas <90166728+tanakaskas@users.noreply.github.com>
2023-02-27 15:35:39 +0900

    Update add_test.py
    test失敗するように編集

--- python/add_test.py
+++ python/add_test.py
@@ -3,7 +3,8 @@ import add
 
 class SampleTest(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add.add(1, 2), 3)
+#         self.assertEqual(add.add(1, 2), 3)
+        self.assertEqual(add.add(1, 2), 5)
 
 if __name__ == "__main__":
     unittest.main()
